d7167b98a8fde8e263a0434cda44ff5f698f1459
Skichko_Bogdan <Bogdan123b@meta.ua>
2024-12-28 23:02:07 +0100

    End

---
+++ Js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    const parsedPadding = Number.parseFloat(padding);
+    const parsedBorder = Number.parseFloat(border);
+    const parsedContent = Number.parseFloat(content);
+    return parsedContent + parsedPadding * 2 + parsedBorder * 2;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
